748ba0a9b32e527ebb066841602a13df93924925
alvendarthy <alvendarthy@gmail.com>
2017-06-06 12:32:23 -0400

    done

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local decode = require "decode"
 local ajuster = require "ajuster"
+local bit = require "bit"
 
 local mem013 = require "memmap.013"
 
@@ -13,6 +14,13 @@ local line
 
 local one_op
 
+local rom = {}
+local label_tab = {}
+local func_tab = {}
+
+local label_max = 0
+local func_max = 0
+
 for line in infile:lines()
 do
 	repeat
@@ -24,10 +32,57 @@ do
 	end
 
 	ajuster.ajust(one_op)
-	print(one_op.op, one_op.arg1, one_op.arg2)
+	--print(one_op.op, one_op.arg1, one_op.arg2)
+	if(one_op.op == "GOTO") then
+		if(label_tab[one_op.arg1] == nil) then
+			label_tab[one_op.arg1] = "LABEL_" .. label_max
+			label_max = label_max + 1
+		end
+		one_op.arg1 = label_tab[one_op.arg1]
+	end
+
+	if(one_op.op == "CALL") then
+		if(func_tab[one_op.arg1] == nil) then
+                        func_tab[one_op.arg1] = "FUNC_" .. func_max
+			func_max = func_max + 1
+                end
+                one_op.arg1 = func_tab[one_op.arg1]
+	end
+	table.insert(rom, one_op)
 	until true
 	
 end
 
-
 infile:close()
+
+local line_num
+
+for line_num, one_op in pairs(rom) do
+	local str = one_op.op
+	offset = line_num - 1
+	repeat
+	if(nil == one_op.arg1) then
+		break
+	end
+	str = string.format("%-10s%4s%-10s", str, "", one_op.arg1)
+
+	if(nil == one_op.arg2) then
+                break
+        end
+	str = string.format("%-24s%-4s%-10s", str, ",", one_op.arg2)
+	until true
+
+	local label_id = label_tab[offset]
+	if(label_id) then
+		print("")
+		print(label_id .. ":")
+	end
+
+	local func_id = func_tab[offset]
+        if(func_id) then
+		print("")
+                print(func_id .. ":")
+        end
+	str = string.format("    %-38s    #0X%s", str, bit.tohex(offset,  4))
+	print(str)
+end
