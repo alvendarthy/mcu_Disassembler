4fb53d37e0756c7675654a2b9090220f952f782a
alvendarthy <alvendarthy@gmail.com>
2017-06-07 05:32:57 -0400

    fix 2nd arg ajust method

--- ajuster.lua
+++ ajuster.lua
@@ -4,11 +4,15 @@ local _M = {}
 local addr_map
 
 function _M.ajust(code)
-	if(code.arg1 and (string.find(code.op, "F$") or string.find(code.op, "FSS$") or string.find(code.op, "FSC$") or string.find(code.op, "FSZ$"))) then
+	if(code.arg1 and (
+		string.find(code.op, "F$") 
+		or string.find(code.op, "FSS$") 
+		or string.find(code.op, "FSC$") 
+		or string.find(code.op, "FSZ$"))) then
 		code.arg1 = addr_map[code.arg1] or code.arg1
 	end
 
-	if(code.arg2)then
+	if(code.arg2 and code.op ~= "BCF" and code.op ~= "BSF")then
 		if(code.arg2 == 1) then
 			code.arg2 = "F"
 		else
